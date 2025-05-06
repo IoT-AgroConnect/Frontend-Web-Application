0789c78a830efb333406b838e26ed209646e2771
Fiorella <U202120418@upc.edu.pe>
2025-05-05 21:29:58 -0500

    Initial Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDkROrKEzexzriGwyg94vvj76NjPqewV9E",
+    authDomain: "agroconnect-demo.firebaseapp.com",
+    projectId: "agroconnect-demo",
+    storageBucket: "agroconnect-demo.appspot.com",
+    messagingSenderId: "981172916098",
+    appId: "1:981172916098:web:1b8a0e8e722615fe7a499b"
+  },
+    baseURL: 'https://web-services-18bk.onrender.com/api/v1',
+
+    //urls for the different endpoints
+    userURL: '/users',
+    authenticationURL: '/authentication',
+    advisorURL: '/advisors',
+    breederURL: '/breeders',
+    resourceURL: '/resources',
+    appointmentURL: '/appointments',
+    expenseURL: '/expenses',
+    notificationURL: '/notifications',
+    publicationURL: '/publications',
+    animalURL: '/animals',
+    cageURL: '/cages',
+    reviewURL: '/reviews',
+    availableDateURL: '/available-dates'
+}
