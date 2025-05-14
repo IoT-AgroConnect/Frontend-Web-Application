e5a3f87f8b76d48b3e94f50e13bf16f720412644
Sebastián Ramírez Méndez <149857901+yorusaurio@users.noreply.github.com>
2025-05-14 16:12:02 -0500

    feat: implement cage editing functionality for US11

--- src/app/management/pages/cage-editor/cage-editor.component.ts
+++ src/app/management/pages/cage-editor/cage-editor.component.ts
@@ -12,19 +12,22 @@ import {MatIcon} from "@angular/material/icon";
 @Component({
   selector: 'app-cage-editor',
   standalone: true,
-    imports: [
-        MatFormField,
-        MatInput,
-        MatButton,
-        FormsModule,
-        MatLabel,
-        MatIcon
-    ],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatButton,
+    FormsModule,
+    MatLabel,
+    MatIcon
+  ],
   templateUrl: './cage-editor.component.html',
   styleUrl: './cage-editor.component.css'
 })
 export class CageEditorComponent implements OnInit {
+  // ID de la jaula a editar (se obtiene desde la URL)
   cageID = 0;
+
+  // Modelo de datos para la jaula
   cage: Cage = {
     id: 0,
     breederId: 0,
@@ -32,20 +35,26 @@ export class CageEditorComponent implements OnInit {
     size: 0,
     observations: ''
   }
-  @ViewChild('cageForm', {static: false})
-  cageForm! : NgForm;
 
-  constructor(private router: Router,
-              private route: ActivatedRoute,
-              private cageService: CageApiService,
-              private snackBar: MatSnackBar) {  }
+  // Referencia al formulario del template
+  @ViewChild('cageForm', { static: false })
+  cageForm!: NgForm;
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private cageService: CageApiService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  // Al iniciar el componente, obtenemos el ID de la jaula desde la URL y cargamos sus datos
   ngOnInit() {
     this.cageID = +this.route.snapshot.paramMap.get('id')!;
     this.getCage(this.cageID);
   }
 
-  getCage(id: number){
+  // Método para obtener los datos de la jaula desde el servicio
+  getCage(id: number) {
     this.cageService.getOne(id).subscribe((data) => {
       this.cage = {
         id: data.id,
@@ -53,21 +62,24 @@ export class CageEditorComponent implements OnInit {
         name: data.name,
         size: data.size,
         observations: data.observations
-      }
+      };
     });
   }
 
-  onSubmit(){
-    if(this.cageForm.form.valid){
-      this.cageService.update(this.cage.id, this.cage)
-        .subscribe(() => {
+  // Maneja el envío del formulario. Si es válido, actualiza la jaula y redirige.
+  onSubmit() {
+    if (this.cageForm.form.valid) {
+      this.cageService.update(this.cage.id, this.cage).subscribe(() => {
         this.router.navigate(['/criador/mis-animales']);
         this.snackBar.open('Jaula actualizada 🎉', '', { duration: 5000 });
       });
     }
   }
 
-  onCancel(){
+  // Cancela la edición y vuelve a la página anterior
+  onCancel() {
     window.history.back();
   }
 }
+// Este componente permite editar los datos de una jaula existente.
+// Utiliza el servicio CageApiService para obtener y actualizar los datos de la jaula
