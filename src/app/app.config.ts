b23f52c1da6ed574ede9b88d5543090fdb22ebae
JohanMV <U20201C105@UPC.EDU.PE>
2025-07-07 04:56:25 -0500

    IoT monitoring - sensor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import {DatePipe} from "@angular/common";
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {provideHttpClient, withFetch} from "@angular/common/http";
+import { DatePipe } from "@angular/common";
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './shared/interceptors/auth.interceptor'; // 👈 ruta a tu interceptor
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), DatePipe,
-    provideHttpClient(withFetch()), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    DatePipe,
+    provideHttpClient(withInterceptors([authInterceptor]))
+  ]
 };
